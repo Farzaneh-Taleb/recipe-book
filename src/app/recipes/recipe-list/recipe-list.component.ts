9dbd3331da799982cf97f529ca4e80f927b38892
f.taleb <farzanehtaleb11@gmail.com>
2017-07-24 16:54:39 +0430

    10 Course Project - 95 - cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe";
 import {RecipeService} from "../recipe.service";
 
@@ -8,7 +8,7 @@ import {RecipeService} from "../recipe.service";
 })
 export class RecipeListComponent implements OnInit {
 
-@Output() recipeSelected = new EventEmitter<Recipe>() ;
+// @Output() recipeSelected = new EventEmitter<Recipe>() ;
   recipes: Recipe[] = [];
   constructor(private recipeService: RecipeService) { }
 
@@ -16,10 +16,12 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes() ;
   }
 
+/*
   onSelected(recipe: Recipe) {
     console.log('cliiiiiik');
     console.log(recipe);
 this.recipeSelected.emit(recipe);
   }
+*/
 
 }
