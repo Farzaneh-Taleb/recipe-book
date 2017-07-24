d3861e2f42dc005a52b694080f1ed88accf63652
f.taleb <farzanehtaleb11@gmail.com>
2017-07-24 16:23:39 +0430

    10 Course Project -90

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from "../recipe";
 import {ShoppingListService} from "../../shopping-list/shopping-list.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'rb-recipe-detail',
@@ -9,12 +10,22 @@ import {ShoppingListService} from "../../shopping-list/shopping-list.service";
 export class RecipeDetailComponent implements OnInit {
 
  @Input() selectedRecipe: Recipe ;
+ private recipeIndex: number =1;
 
 
-  constructor( private sls: ShoppingListService) { }
+  constructor( private sls: ShoppingListService , private router: Router) { }
 
   ngOnInit() {
   }
+
+  onDelete() {
+    this.router.navigate(['/recipes' , this.recipeIndex , 'edit']);
+
+  }
+   onEdit(){
+     this.router.navigate(['/recipes' , this.recipeIndex , 'edit']);
+
+   }
 
 
   onAddToShoppingList() {
