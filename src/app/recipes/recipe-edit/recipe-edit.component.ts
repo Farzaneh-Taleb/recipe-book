e0c45f4e3110d6a06e24bc9b0f5586cb99f09c48
f.taleb <farzanehtaleb11@gmail.com>
2017-08-16 17:32:06 +0430

    12 Course Project 135 half

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  private recipeIndex: number ;
+  private subscription: Subscription
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) =>
+    );
   }
 
 }
