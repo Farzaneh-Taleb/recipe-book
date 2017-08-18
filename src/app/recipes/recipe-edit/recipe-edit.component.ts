a3fa7a7f62790da6755594de11d34d39df0e6ef3
Farzaneh-Taleb <farzanehtaleb11@gmail.com>
2017-08-18 20:30:40 +0430

    12 Course Project - 136-137

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {RecipeService} from '../recipe.service';
 import {Subscription} from 'rxjs/Subscription';
+import {Recipe} from '../recipe';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms' ;
 
 @Component({
   selector: 'rb-recipe-edit',
@@ -9,28 +11,63 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit,OnDestroy {
-  private recipeIndex: number ;
-  private subscription: Subscription
+  recipeForm: FormGroup;
+  private recipeIndex: number;
+  private subscription: Subscription;
+  private isNew = true;
+  private recipe: Recipe;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService , private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
-    let isNew = true ;
+
     this.subscription = this.route.params.subscribe(
       (params: any) => {
-        if (params.hasOwnProperty('id')){
-          isNew = false;
-          this.recipeIndex = +params['id'] ;
+        if (params.hasOwnProperty('id')) {
+          this.isNew = false;
+          this.recipeIndex = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
         } else {
-          isNew = true ;
+          this.isNew = true;
+          this.recipe = null;
         }
-        console.log('isNew: ' , isNew);
+        console.log('isNew: ', this.isNew);
+     this.initForm() ;
       }
     );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe() ;
+    this.subscription.unsubscribe();
+  }
+
+  private  initForm() {
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeContent = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
+
+    if (!this.isNew) {
+      for (let i = 0; i < this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, [Validators.required, Validators.pattern('\\d+')])
+          })
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeImageUrl = this.recipe.imagePath;
+      recipeContent = this.recipe.description;
+    }
+      this.recipeForm = this.formBuilder.group({
+  name: [recipeName , Validators.required],
+  imagePath: [recipeImageUrl , Validators.required],
+  description: [recipeContent , Validators.required],
+  ingredients: recipeIngredients
+}) ;
   }
 
 }
